52a1f071dd9ef6264e82a3c5ec9472664c5d31b6
Vivian Desktop <visasal88@gmail.com>
2023-08-09 19:39:40 -0300

    feat: create and apply theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
 import './index.css'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
