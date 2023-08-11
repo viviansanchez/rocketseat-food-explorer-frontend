2e9d9ffddf807f37143a5a41fe34937b84dbfd09
Vivian Desktop <visasal88@gmail.com>
2023-08-11 13:08:48 -0300

    feat: add components, styling and media queries to SigIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -4,26 +4,21 @@ import { Logo } from "../../components/Logo";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
-import { PiX } from 'react-icons/pi'
-
 export function SignIn() {
 
   return(
     <Container>
-      
       <Logo />
-      <Form>
-       
 
-        {/* input
-        input 
-        button 
-        link (react router dom) */}
-        <Input label={"testando"} placeholder={"placeholder"} />
-        <Input label={"outro teste"} icon={PiX} />
+      <Form>
+        <h1>Faça login</h1>
+        <Input label={"Email"} placeholder={"Exemplo: exemplo@exemplo.com.br"} />
+        <Input label={"Senha"} placeholder={"No mínimo 6 caracteres"} />
         <Button
         title={"Entrar"} 
         />
+
+        {/*link (react router dom) */}
       </Form>
     </Container>
   )
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,19 +3,47 @@ import styled from 'styled-components'
 export const Container = styled.div`
   padding-top: 9.875rem;
   margin: 0 auto;
+  height: 100vh;
   
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  border: 1px solid red;
+  @media only screen and (min-width: 768px){
+    flex-direction: row;
+    justify-content: center;
+    gap: 19.125rem;
+    
+    padding-top: 0;
+ }
 `
 
 export const Form = styled.form`
  margin-top: 4.625rem;
- width: 400px;
+ width: 19.75rem;
 
- border: 1px solid green;
+ @media only screen and (min-width: 768px){
+  width: 29.75rem;
+  padding: 4rem;
+  border-radius: 1rem;
+  background-color: ${({theme}) => theme.COLORS.DARK_700};
+ }
+
+ h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+
+    color: ${({theme}) => theme.COLORS.LIGHT_100};
+    font-size: 2rem;
+    font-weight: 500;
+    line-height: 2.8rem;
+
+    display: none;
+
+    @media only screen and (min-width: 768px) {
+      display: block;
+    }
+ }
 `
 
 
