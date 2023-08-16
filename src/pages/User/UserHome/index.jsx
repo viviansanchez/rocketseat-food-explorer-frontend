49dd71751fe40d5f26b5512bc8caadc114833ef3
Vivian Desktop <visasal88@gmail.com>
2023-08-16 18:05:19 -0300

    update skeleton of UserHome page

--- src/pages/User/UserHome/index.jsx
+++ src/pages/User/UserHome/index.jsx
@@ -2,13 +2,17 @@ import { Container, Main } from "./styles"
 
 import { Footer } from "../../../components/Footer"
 import { MobileNavbar } from "../../../components/MobileNavbar"
+import { DesktopNavbar } from "../../../components/DesktopNavbar"
+import { Banner } from "../../../components/Banner"
 
 export function UserHome() {
   return(
     <Container>
       <MobileNavbar />
+      {/* <DesktopNavbar isAdmin /> */}
+      
       <Main>
-        {/* cta component */}
+        <Banner className="banner" />
         {/* section 1 - refeições */}
         {/* section 2 - sobremesas */}
         {/* section 3 - bebidas */}
--- src/pages/User/UserHome/styles.js
+++ src/pages/User/UserHome/styles.js
@@ -10,9 +10,39 @@ grid-template-areas:
  "main"
  "footer"
 ;
+
+/* .hide {
+  display: none;
+}
+
+.show {
+  display: block;
+}
+
+@media only screen and (min-width: 768px) {
+  .hide {
+    display: block;
+  }
+
+  .show {
+    display: none;
+  }
+} */
 `
 
 export const Main = styled.main`
   grid-area: main;
-  overflow-y: auto;
+  /* overflow-y: auto; acho que nao precisa disso at all*/
+
+  width: 100%;
+  margin: 0 auto;
+  padding: 2.75rem 1rem 1.5rem;
+
+  border: 1px solid red;
+
+  @media only screen and (min-width: 768px){
+    padding: 10.25rem 7.75rem 2.938rem;
+  }
 `
+
+//da pra usar classe hide e show com display none e display block pra deixar o navbar desktop escondido e ai trocar com media query (a partir de tal mobile vira display none e desktop vira display block)
