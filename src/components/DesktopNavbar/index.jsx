6aa45217f1c8874852f6e05736fd57349373c783
Vivian Desktop <visasal88@gmail.com>
2023-08-16 13:43:25 -0300

    feat: create DesktopNavbar component

---
+++ src/components/DesktopNavbar/index.jsx
@@ -0,0 +1,25 @@
+import { Container } from "./styles";
+
+import { FiLogOut } from 'react-icons/fi'
+import { PiReceipt, PiMagnifyingGlassBold } from "react-icons/pi";
+
+import { NavbarLogo } from "../NavbarLogo";
+import { Input } from "../Input"
+import { Button } from "../Button";
+
+
+export function DesktopNavbar({ isAdmin = false }) {
+
+  return(
+    <Container> 
+      <NavbarLogo isAdmin={isAdmin} />
+      <Input className="input" icon={PiMagnifyingGlassBold} placeholder="Busque por pratos ou ingredientes" />
+      <Button className="button" title={"Pedidos (0)"} icon={PiReceipt}/>
+      <a href="#" className="logout">
+        <FiLogOut />
+      </a>
+      {/* usenavigate aqui */}
+    </Container>
+  )
+
+}
---
+++ src/components/DesktopNavbar/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const Container = styled.nav`
+  grid-area: navbar;
+  height: 7.125rem;
+  padding: 1.5rem 7.688rem;
+
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+
+  background-color:  ${({theme}) => theme.COLORS.DARK_700};
+
+  > div:nth-child(2){
+    width: 36.313rem;
+    margin-bottom: 0;
+
+     > div:nth-child(1){
+      margin-top: 0;
+     }
+  }
+
+  .button {
+    width: 13.5rem;    
+  }
+
+  .logout {
+    width: 2rem;
+    height: 2rem;
+    color: ${({theme}) => theme.COLORS.LIGHT_100};
+
+    svg {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+
+  @media only screen and (min-width: 768px) and (max-width: 1260px) {
+    padding: 1.5rem 1rem;
+    gap: 0.5rem;
+
+    div:nth-child(2){
+      width: 20rem;
+    }
+  }
+`
