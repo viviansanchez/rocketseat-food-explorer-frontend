035ca99e269bbde4ea1346650ecbf515c5bb3cd8
Vivian Desktop <visasal88@gmail.com>
2023-08-17 19:36:26 -0300

    feat: add components to UserHome page -> mockup of page is done

--- src/pages/User/UserHome/index.jsx
+++ src/pages/User/UserHome/index.jsx
@@ -1,21 +1,41 @@
-import { Container, Main } from "./styles"
+import { Container, Main, Meals, Desserts, Drinks } from "./styles"
 
-import { Footer } from "../../../components/Footer"
 import { MobileNavbar } from "../../../components/MobileNavbar"
 import { DesktopNavbar } from "../../../components/DesktopNavbar"
 import { Banner } from "../../../components/Banner"
+import { SectionTitle } from "../../../components/SectionTitle"
+import { Card } from "../../../components/Card"
+import { Footer } from "../../../components/Footer"
 
 export function UserHome() {
   return(
     <Container>
       <MobileNavbar />
-      {/* <DesktopNavbar isAdmin /> */}
+      {/* <DesktopNavbar isAdmin /> this is here for later - change navbar based on screen (breakpoint chosen for this app is 768px) */}
       
       <Main>
         <Banner className="banner" />
-        {/* section 1 - refeições */}
-        {/* section 2 - sobremesas */}
-        {/* section 3 - bebidas */}
+        {/* <Card isAdmin />
+        <Card /> */}
+
+        <Meals>
+          <SectionTitle title={"Refeições"} />
+          <Card />
+        </Meals>
+
+        <Desserts>
+          <SectionTitle title={"Sobremesas"} />
+          <Card isAdmin />
+        </Desserts>
+
+        <Drinks>
+          <SectionTitle title={"Bebidas"} />
+          <Card />
+        </Drinks>
+
+        {/* in each section instead of a single card, will be Swiper lib! */}
+        
+        {/* maybe later change this and create a section component if possible? depends on how it will works with the api. Leaving like this for now, but will leave comment here for later */}
       </Main>
       <Footer />
     </Container>
--- src/pages/User/UserHome/styles.js
+++ src/pages/User/UserHome/styles.js
@@ -1,48 +1,69 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-height: 100vh;
+  height: 100vh;
 
-display: grid;
-grid-template-rows: 7.125rem auto 4.875rem;
-grid-template-areas: 
- "navbar"
- "main"
- "footer"
-;
+  display: grid;
+  grid-template-rows: 7.125rem auto 4.875rem;
+  grid-template-areas: 
+  "navbar"
+  "main"
+  "footer"
+  ;
 
-/* .hide {
-  display: none;
-}
-
-.show {
-  display: block;
-}
-
-@media only screen and (min-width: 768px) {
-  .hide {
-    display: block;
+  /* .hide {
+    display: none;
   }
 
   .show {
-    display: none;
+    display: block;
   }
-} */
+
+  @media only screen and (min-width: 768px) {
+    .hide {
+      display: block;
+    }
+
+    .show {
+      display: none;
+    }
+  } */
 `
 
 export const Main = styled.main`
   grid-area: main;
-  /* overflow-y: auto; acho que nao precisa disso at all*/
 
   width: 100%;
   margin: 0 auto;
   padding: 2.75rem 1rem 1.5rem;
 
-  border: 1px solid red;
-
   @media only screen and (min-width: 768px){
     padding: 10.25rem 7.75rem 2.938rem;
   }
+`
+
+export const Meals = styled.section`
+  margin-top: 3.875rem;
+  margin-bottom: 1.5rem;
+
+  @media only screen and (min-width: 768px){
+    margin-top: 3.063rem;
+    margin-bottom: 3rem;
+
+  }
+`
+
+export const Desserts = styled.section`
+  margin-bottom: 1.5rem;
+
+  @media only screen and (min-width: 768px){
+    margin-bottom: 3rem;
+
+  }
+`
+
+export const Drinks = styled.section`
+
 `
 
 //da pra usar classe hide e show com display none e display block pra deixar o navbar desktop escondido e ai trocar com media query (a partir de tal mobile vira display none e desktop vira display block)
