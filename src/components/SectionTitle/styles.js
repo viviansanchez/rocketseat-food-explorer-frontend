8c1ffecea7868eaef7627fb75b35b91f570f1ba0
Vivian Desktop <visasal88@gmail.com>
2023-08-17 19:20:47 -0300

    style: add margin bottom to component

--- src/components/SectionTitle/styles.js
+++ src/components/SectionTitle/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.h2`
   color: ${({theme}) => theme.COLORS.LIGHT_300};
   font-weight: 500;
   font-size: 1.125rem;
+  margin-bottom: 1.5rem;
 
   @media only screen and (min-width: 768px){
     font-size: 2rem;
