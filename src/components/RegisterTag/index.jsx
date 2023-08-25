54d9c89167d9d3083f8f36d6d5f1ead70b7eaf37
Vivian Desktop <visasal88@gmail.com>
2023-08-25 16:53:02 -0300

    feat: create RegisterTags component

---
+++ src/components/RegisterTag/index.jsx
@@ -0,0 +1,23 @@
+import { Container } from "./styles";
+
+import { PiXBold, PiPlusBold } from 'react-icons/pi'
+
+export function RegisterTag({ isNew, value, onClick, ...rest }) {
+  return(
+    <Container isNew={isNew}>
+      <input
+        type="text"
+        value={value}
+        readOnly={!isNew}
+        {...rest}
+      />
+
+      <button
+        type="button"
+        onClick={onClick}
+      >
+        {isNew ? <PiPlusBold /> : <PiXBold />}
+      </button>
+    </Container>
+  )
+}
---
+++ src/components/RegisterTag/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 0.625rem 1rem; 
+  border-radius: 0.5rem;
+  max-width: 10rem;
+
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme, isNew }) => isNew ? "transparent" : theme.COLORS.LIGHT_600};
+  border: ${({ theme, isNew }) => isNew ? `1px dashed ${theme.COLORS.LIGHT_500}` : "none"};
+
+  > input, button {
+    border: none;
+    background: none;
+
+    color: ${({ theme, isNew }) => isNew ? theme.COLORS.LIGHT_500 : theme.COLORS.LIGHT_100};
+  }
+
+  > input {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    outline: none;
+    width: 100%; 
+  }
+
+  > button {
+    height: 1rem;
+    width: 1rem;
+    
+    svg {
+      height: 1rem;
+      width: 1rem;
+    }
+  }
+`
