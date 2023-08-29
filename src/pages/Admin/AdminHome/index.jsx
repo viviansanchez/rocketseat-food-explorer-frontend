dd007d26230b1baa689cbbc99e5084307ec3e1cc
Vivian Desktop <visasal88@gmail.com>
2023-08-29 14:08:28 -0300

    add Header component, add correct titles for Carousel components

--- src/pages/Admin/AdminHome/index.jsx
+++ src/pages/Admin/AdminHome/index.jsx
@@ -1,7 +1,6 @@
 import { Container, Main } from "./styles"
 
-import { MobileNavbar } from "../../../components/MobileNavbar"
-import { DesktopNavbar } from "../../../components/DesktopNavbar"
+import { Header } from "../../../components/Header"
 import { Banner } from "../../../components/Banner"
 import { Carousel } from "../../../components/Carousel"
 import { Footer } from "../../../components/Footer"
@@ -9,15 +8,14 @@ import { Footer } from "../../../components/Footer"
 export function AdminHome() {
   return(
     <Container>
-      <MobileNavbar isAdmin />
-      {/* <DesktopNavbar isAdmin /> this is here for later - change navbar based on screen (breakpoint chosen for this app is 768px) */}
+      <Header isAdmin />
       
       <Main>
         <Banner className="banner" />
 
-        <Carousel title={"Teste!!!"} isAdmin/>
-        <Carousel className="margin-top" title={"Teste!!!"} isAdmin/>
-        <Carousel className="margin-top" title={"Teste!!!"} isAdmin/>
+        <Carousel title="Refeições" isAdmin/>
+        <Carousel className="margin-top" title="Sobremesas" isAdmin/>
+        <Carousel className="margin-top" title="Bebidas" isAdmin/>
 
       </Main>
       <Footer />
--- src/pages/Admin/AdminHome/styles.js
+++ src/pages/Admin/AdminHome/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   display: grid;
   grid-template-rows: 7.125rem auto 4.875rem;
   grid-template-areas: 
-  "navbar"
+  "header"
   "main"
   "footer"
   ;
