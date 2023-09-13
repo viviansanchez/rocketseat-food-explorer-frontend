4e360f56c4edd169fd42551ee10047f27338bd2c
Vivian Desktop <visasal88@gmail.com>
2023-09-13 18:41:58 -0300

    feat: change regular link to react router dom Link component, add effect on hover to be a better indication for the user that the title is clickable

--- src/components/CardDishTitle/styles.js
+++ src/components/CardDishTitle/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export const Container = styled.a`
+export const Container = styled(Link)`
   color: ${({theme}) => theme.COLORS.LIGHT_300};
   font-size: 0.875rem;
   font-weight: 500;
@@ -9,14 +10,15 @@ export const Container = styled.a`
   align-items: center;
   gap: 0.1rem;
 
+  transition: scale 400ms, color 400ms;
+
+  &:hover {
+    scale: 1.1;
+    color: ${({theme}) => theme.COLORS.CAKE_200};
+  }
+
   @media only screen and (min-width:768px){
     font-size: 1.5rem;
     font-weight: 700;
   }
 `
-
-
-
-
-// ver a partir da pg 141 do stage 09 
-//styled(Link) importando Link de react-router-dom aqui! pg 145
