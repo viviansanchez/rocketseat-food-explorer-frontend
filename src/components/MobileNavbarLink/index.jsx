27103b6afbaeb1caf0f6ea0fcd48944c1cc508f2
Vivian Desktop <visasal88@gmail.com>
2023-09-14 14:40:50 -0300

    style: remove comment

--- src/components/MobileNavbarLink/index.jsx
+++ src/components/MobileNavbarLink/index.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
   export function MobileNavbarLink({ title, page }) {
   return(
     <Container>
-      {/* <a href={page}>{title}</a> */}
       <Link to={page}>{title}</Link>
     </Container>
   )
