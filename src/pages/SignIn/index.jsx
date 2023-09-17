5d9ae12974ff0b5ebb463bb92b9516a4ebf12f61
Vivian Desktop <visasal88@gmail.com>
2023-09-17 16:28:23 -0300

    feat: add context usage for user to be able to sign in

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Container, Form } from "./styles";
 
 import { Logo } from "../../components/Logo";
@@ -6,7 +8,17 @@ import { Input } from "../../components/Input";
 
 import { Link } from "react-router-dom";
 
+import { useAuth } from "../../hooks/auth";
+
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { SignIn } = useAuth()
+
+  function handleSignIn() {
+    SignIn({ email, password })
+  }
 
   return(
     <Container>
@@ -14,18 +26,24 @@ export function SignIn() {
 
       <Form>
         <h1>Faça login</h1>
+
         <Input 
           type="email"
           label={"Email"} 
           placeholder={"Exemplo: exemplo@exemplo.com.br"} 
+          onChange={e => setEmail(e.target.value)}
         />
+
         <Input 
           type="password"
           label={"Senha"} 
           placeholder={"No mínimo 6 caracteres"} 
+          onChange={e => setPassword(e.target.value)}
         />
+        
         <Button
-        title={"Entrar"} 
+          title={"Entrar"} 
+          onClick={handleSignIn}
         />
 
         <Link to={"/register"}>
