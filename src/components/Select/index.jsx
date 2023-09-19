effaa3b077128c593273bd19a35e25008775539b
Vivian Desktop <visasal88@gmail.com>
2023-09-19 19:53:19 -0300

    adjust select component to receive state and onChange

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -2,17 +2,21 @@ import { Container } from "./styles";
 
 import { useId } from "react";
 
-export function Select({ label, className }) {
+export function Select({ label, className, state, onChange }) {
   const id = useId()
 
   return(
     <Container className={className}>
       <label htmlFor={id}>{label}</label>
-      <select id={id}>
-        <option selected disabled>-- escolha uma categoria --</option>
+      <select 
+        id={id}
+        onChange={onChange}
+        defaultValue={state}
+      >
+        <option value="" disabled>-- escolha uma categoria --</option> 
         <option value="meal">Refeições</option>
         <option value="dessert">Sobremesas</option>
-        <option value="drinks">Bebidas</option>
+        <option value="drink">Bebidas</option>
       </select>
     </Container>
   )
