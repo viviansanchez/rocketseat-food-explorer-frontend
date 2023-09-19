4ab3e9db7e5b5a8f02cf66565e19a187bd1098eb
Vivian Desktop <visasal88@gmail.com>
2023-09-19 15:29:48 -0300

    implement new recommended way (axios) to insert auth token in request

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -14,7 +14,7 @@ function AuthProvider({ children }) {
       localStorage.setItem("@foodexplorer:user", JSON.stringify(user))
       localStorage.setItem("@foodexplorer:token", token)
 
-      api.defaults.headers.authorization = `Bearer: ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer: ${token}`
       setData({ user, token })
 
     } catch(err) {
@@ -38,7 +38,7 @@ function AuthProvider({ children }) {
     const user = localStorage.getItem("@foodexplorer:user")
 
     if(token && user){
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
       setData({
         token,
