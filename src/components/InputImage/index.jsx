d5207b291515c7923b62c5ab345671737c4f2444
Vivian Desktop <visasal88@gmail.com>
2023-09-20 15:51:23 -0300

    feat: add onChange prop to input

--- src/components/InputImage/index.jsx
+++ src/components/InputImage/index.jsx
@@ -4,14 +4,18 @@ import { PiUploadSimple } from 'react-icons/pi'
 
 import { useId } from "react";
 
-export function InputImage({ label, description, className }){
+export function InputImage({ label, description, className, onChange }){
   const id = useId()
 
   return(
     <Container className={className}>
       <label htmlFor={id}>
         {label}
-        <input type="file" id={id} />
+        <input 
+          type="file" 
+          id={id} 
+          onChange={onChange}
+        />
         <div>
           <PiUploadSimple />
           <p>{description}</p>
