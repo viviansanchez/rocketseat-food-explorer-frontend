efedc81b9cbb7f5fc5fd5f329c13463143b0097a
Vivian Desktop <visasal88@gmail.com>
2023-09-21 15:12:07 -0300

    add onChange prop to pass to headers

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,11 +3,11 @@ import { Container } from "./styles";
  import { HeaderMobile } from "../HeaderMobile";
 import { HeaderDesktop } from "../HeaderDesktop";
 
-export function Header({ isAdmin = false }) {
+export function Header({ isAdmin = false, onChange }) {
   return(
     <Container>
-      <HeaderMobile isAdmin={isAdmin} className="mobile" />
-      <HeaderDesktop isAdmin={isAdmin} className="desktop" />
+      <HeaderMobile isAdmin={isAdmin} onChange={onChange} className="mobile" />
+      <HeaderDesktop isAdmin={isAdmin} onChange={onChange} className="desktop" />
     </Container>
   )
 }
