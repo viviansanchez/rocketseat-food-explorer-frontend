f8d9f963e243aab398d17208d4d614cd597fd5e6
Vivian Desktop <visasal88@gmail.com>
2023-09-21 15:14:07 -0300

    feat: add logic to display dishes based on search and category

--- src/pages/Admin/AdminHome/index.jsx
+++ src/pages/Admin/AdminHome/index.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react"
+
+import { api } from "../../../services/api"
+
 import { Container, Main } from "./styles"
 
 import { Header } from "../../../components/Header"
@@ -6,16 +10,57 @@ import { Carousel } from "../../../components/Carousel"
 import { Footer } from "../../../components/Footer"
 
 export function AdminHome() {
+  const [search, setSearch] = useState("")
+  const [dishes, setDishes] = useState([])
+  const [meals, setMeals] = useState([])
+  const [drinks, setDrinks] = useState([])
+  const [desserts, setDesserts] = useState([])
+
+  useEffect(() => {
+    console.log(`${search}`)
+    async function fetchDishes(){
+      const response = await api.get(`/dishes?search=${search}`)
+      setDishes(response.data)
+
+      setMeals(dishes.filter((dish) => dish.category === "meal"))
+      setDesserts(dishes.filter(dish => dish.category === "dessert"))
+      setDrinks(dishes.filter(dish => dish.category === "drink"))
+    }
+
+    fetchDishes()
+  }, [search])
+
   return(
     <Container>
-      <Header isAdmin />
+      <Header 
+        isAdmin 
+        onChange={e => setSearch(e.target.value)}
+      />
       
       <Main>
         <Banner className="banner" />
 
-        <Carousel title="Refeições" isAdmin/>
-        <Carousel className="margin-top" title="Sobremesas" isAdmin/>
-        <Carousel className="margin-top" title="Bebidas" isAdmin/>
+        <Carousel 
+          title="Refeições" 
+          isAdmin 
+          dishes={meals}
+        />
+
+        <Carousel 
+          className="margin-top" 
+          title="Sobremesas" 
+          isAdmin
+          
+          dishes={desserts}
+        />
+
+        <Carousel 
+          className="margin-top" 
+          title="Bebidas" 
+          isAdmin
+          
+          dishes={drinks}
+        />
 
       </Main>
       <Footer />
