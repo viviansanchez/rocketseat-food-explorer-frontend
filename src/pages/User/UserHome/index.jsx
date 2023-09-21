aa072b7e091fe8e2ec0a226bfe90e622614ea00e
Vivian Desktop <visasal88@gmail.com>
2023-09-21 15:53:28 -0300

    feat: add functionality to search and display dishes based on category

--- src/pages/User/UserHome/index.jsx
+++ src/pages/User/UserHome/index.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react"
+
+import { api } from "../../../services/api"
+
 import { Container, Main } from "./styles"
 
 import { Header } from "../../../components/Header"
@@ -6,18 +10,37 @@ import { Carousel } from "../../../components/Carousel"
 import { Footer } from "../../../components/Footer"
 
 export function UserHome() {
+  const [search, setSearch] = useState("")
+  const [dishes, setDishes] = useState([])
+  const [meals, setMeals] = useState([])
+  const [drinks, setDrinks] = useState([])
+  const [desserts, setDesserts] = useState([])
+
+  useEffect(() => {
+    async function fetchDishes(){
+      const response = await api.get(`/dishes?search=${search}`)
+      setDishes(response.data)
+
+      setMeals(dishes.filter((dish) => dish.category === "meal"))
+      setDesserts(dishes.filter(dish => dish.category === "dessert"))
+      setDrinks(dishes.filter(dish => dish.category === "drink"))
+    }
+
+    fetchDishes()
+  }, [search])
+
   return(
     <Container>
-      <Header />
+      <Header onChange={e => setSearch(e.target.value)} />
       
       <Main>
         <Banner className="banner" />
 
-        <Carousel title="Refeições"/>
+        <Carousel title="Refeições" dishes={meals}/>
           
-        <Carousel className="margin-top" title="Sobremesas"/>
+        <Carousel className="margin-top" title="Sobremesas" dishes={desserts}/>
         
-        <Carousel className="margin-top" title="Bebidas"/>
+        <Carousel className="margin-top" title="Bebidas" dishes={drinks}/>
 
       </Main>
       <Footer />
