2c6654edf8e3a1f11652c0cd3bc3974e0b26e89c
Vivian Desktop <visasal88@gmail.com>
2023-09-22 16:44:34 -0300

    feat: add update dish an delete dish funcionality

--- src/pages/Admin/EditDish/index.jsx
+++ src/pages/Admin/EditDish/index.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { api } from "../../../services/api";
+
 import { Container, Main, Form } from "./styles";
 
 import { Header } from '../../../components/Header'
+import { PreviousPageNavigate } from "../../../components/PreviousPageNavigate"
 import { InputImage } from "../../../components/InputImage";
 import { Input } from '../../../components/Input'
 import { Select } from '../../../components/Select'
@@ -11,13 +16,124 @@ import { Button } from '../../../components/Button'
 import { Footer } from '../../../components/Footer'
 
 export function EditDish() {
+  const [data, setData] = useState(null)
+  const [image, setImage] = useState(null)
+  const [title, setTitle] = useState("")
+  const [category, setCategory] = useState("")
+  const [ingredients, setIngredients] = useState([])
+  const [newIngredient, setNewIngredient] = useState("")
+  const [price, setPrice] = useState("")
+  const [description, setDescription] = useState("")
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleAddIngredient() {
+    const newIngredientObject = { id: null, name: newIngredient, dish_id: data.id } 
+
+    setIngredients(prevState => [...prevState, newIngredientObject])
+    setNewIngredient("")
+    
+  }
+
+  function handleRemoveIngredient(deleted) {
+    setIngredients(prevState => prevState.filter(ingredient => ingredient !== deleted))
+  }
+
+  async function handleUpdateDish() {
+    if(typeof image == "string"){
+      return alert ("É necessário refazer o upload da imagem atual, ou fazer upload de uma nova imagem")
+    }
+
+    if(!image) {
+      return alert("É necessário inserir uma foto do prato")
+    } 
+
+    if(!title) {
+      return alert("É necessário digitar o nome do prato")
+    }
+
+    if(category == "") {
+      return alert("É necessário escolher a categoria do prato")
+    }
+
+    if(newIngredient) {
+      return alert("Você deixou um ingrediente no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio")
+    }
+
+    if(ingredients.length === 0){
+      return alert("É necessário adicionar ao menos um ingrediente")
+    }
+
+    if(!price) {
+      return alert("É necessário digitar o preço do prato")
+    }
+
+    if(!description) {
+      return alert("É necessário digitar a descrição do prato")
+    }
+
+    const ingredientsNames = ingredients.map(ingredient => ingredient.name)
+    console.log(ingredientsNames)
+
+    const formData = new FormData()
+
+    formData.append("image", image)
+    formData.append("title", title)
+    formData.append("category", category)
+    formData.append("ingredients", ingredientsNames)
+    formData.append("price", price)
+    formData.append("description", description)
+
+    try {
+      await api.put(`/dishes/${params.id}`, formData)
+      alert("Prato editado com sucesso!")
+      navigate("/")
+    } catch(err) {
+      if(err.response){
+        alert(err.response.data.message)
+      } else {
+        alert("Não foi possível cadastrar o prato")
+      }
+    }
+  }
+
+  async function handleDeleteDish() {
+    const confirm = window.confirm("Deseja realmente remover este prato? Esta ação não pode ser desfeita.")
+
+    if(confirm) {
+      await api.delete(`/dishes/${params.id}`)
+      navigate("/")
+    }
+  }
+
+  useEffect(() => {
+    async function fetchDish() {
+
+      try {
+      const response = await api.get(`dishes/${params.id}`)
+      setData(response.data)
+      setImage(response.data.image)
+      setTitle(response.data.title)
+      setCategory(response.data.category)
+      setIngredients(response.data.ingredients)
+      setPrice(response.data.price)
+      setDescription(response.data.description)
+
+      } catch(err) {
+        return console.error(err.message)
+      }
+    }
+
+    fetchDish()
+  }, [])
+
   return(
     <Container>
       <Header isAdmin />
 
       <Main>
-        {/* PreviousPageLink component - create when routing is done */}
-        <a href="#">VOLTAR</a>
+        <PreviousPageNavigate />
 
         <h1>Editar prato</h1>
 
@@ -26,21 +142,42 @@ export function EditDish() {
             label="Imagem do prato" 
             description="Selecione imagem para alterá-la"
             className="grid-image"
+            onChange={e => setImage(e.target.files[0])}
           />
 
           <Input 
             className="change-input-style grid-name" 
             label="Nome" 
-            placeholder="Salada Ceaser" 
+            placeholder={"Ex: Salada Ceasar"} 
+            value={title}
+            onChange={e => setTitle(e.target.value)}
             type="text"
           />
 
-          <Select className="grid-category" label="Categoria" />
+          <Select 
+            className="grid-category" 
+            label="Categoria" 
+            state={category}
+            onChange={e => setCategory(e.target.value)}
+          />
           
           <FormSection className="grid-ingredients ingredients" title="Ingredientes">
             <div>
-              <RegisterTag value={"Pão Naan"} />
-              <RegisterTag isNew />
+            {
+              ingredients.map((ingredient, index) => (
+                <RegisterTag 
+                  key={index}
+                  value={ingredient.name}
+                  onClick={() => handleRemoveIngredient(ingredient)}
+                />
+                ))
+            }
+              <RegisterTag 
+                isNew
+                value={newIngredient}
+                onChange={e => setNewIngredient(e.target.value)}
+                onClick={handleAddIngredient}
+              />
             </div>
           </FormSection>
 
@@ -48,18 +185,22 @@ export function EditDish() {
             className="change-input-style grid-price" 
             label="Preço" 
             placeholder="R$40,00"
+            value={price}
+            onChange={e => setPrice(e.target.value)}
             type="text"
           />
 
           <TextArea 
+            className="grid-textarea" 
             label="Descrição" 
             placeholder="A Salada Ceasar é uma opção refrescante para o verão" 
-            className="grid-textarea" 
+            value={description}
+            onChange={e => setDescription(e.target.value)}
           />
           
           <div className="grid-btn btn-wrapper">
-            <Button title="Excluir prato"/>
-            <Button title="Salvar alterações"/>
+            <Button title="Excluir prato" onClick={handleDeleteDish}/>
+            <Button title="Salvar alterações" onClick={handleUpdateDish}/>
           </div>
         </Form>
 
